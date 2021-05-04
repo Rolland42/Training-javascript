74efb4a02ec2d8aa509653cf60b51acdbea4c0d8
sonia <s.rolland@codeur.online>
2021-05-04 09:57:39 +0200

    two

---
+++ training1/main.js
@@ -0,0 +1,14 @@
+/* let str = "e";
+let res = str.replace(/e/g , "€");
+
+document.getElementById('str').innerHTML = str;
+document.getElementById('res').innerHTML = res; */
+
+/* let calcul = function(nombre){
+    return nombre*nombre; 
+} */
+function calcul(nombre){
+    return nombre*nombre; 
+ }
+ let x = calcul(5);
+ document.getElementById('str').innerHTML = x;
