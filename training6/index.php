53a2599fb73603266aadab351be49d6ca9b0244d
sonia <s.rolland@codeur.online>
2021-05-04 10:21:50 +0200

    pdate2

---
+++ training6/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Objet constructeur</title>
+    <script src="draft1.js"></script>
+</head>
+<body>
+    <h1>titre principal</h1>
+    <p>Un paragraphe</p>
+    <p id='p1'></p>
+    <p id='p2'></p>
+    <p id='p3'></p>
+    <p id='p4'></p>
+
+</body>
+</html>
