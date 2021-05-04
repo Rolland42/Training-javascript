ec2e5d02441891af915ef96928c7932547caceaf
sonia <s.rolland@codeur.online>
2021-05-04 10:24:22 +0200

    update3

---
+++ training7/main.js
@@ -0,0 +1,14 @@
+//Objet Javascript
+let utlisateur = {
+
+    "prenom" : "Sonie",
+    "nom" : "Lovers",
+    "adresse" : {
+    "rue" : "1 rue du miroir" ,
+    "cp" :  21000,
+    "ville" : "Dijon",
+    "pays"  : "France",
+},
+
+
+}
